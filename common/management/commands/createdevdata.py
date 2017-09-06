201ee6f6e04ea16ae5df31a9e1dc1e8850fb0838
racheltstevens <racheltstevens@gmail.com>
2017-09-06 11:11:33 -0400

    Add createdevdata management command
    * Creates homepage
    * calls management commands from other apps
    * creates a superuser if one does not exist

---
+++ common/management/commands/createdevdata.py
@@ -0,0 +1,60 @@
+from home.models import HomePage
+
+from wagtail.wagtailcore.models import Page, Site
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from django.core import management
+
+
+class Command(BaseCommand):
+    help = 'Creates data appropriate for development'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--delete',
+            action='store_true',
+            dest='delete',
+            default=False,
+            help='Delete homepage and child pages before creating new data.',
+        )
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        if options['delete']:
+            Page.objects.filter(slug='home').delete()
+
+        try:
+            HomePage.objects.get(slug='home')
+        except ObjectDoesNotExist:
+            Page.objects.filter(slug='home').delete()
+            home_page = HomePage(title='Home', slug='home')
+
+            root_page = Page.objects.get(title='Root')
+            root_page.add_child(instance=home_page)
+
+            Site.objects.create(
+                site_name='SecureDrop.org (Dev)',
+                hostname='localhost',
+                port='8000',
+                root_page=home_page,
+                is_default_site=True
+            )
+
+        management.call_command('createblogdata', '10')
+
+        # Create superuser
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                'test',
+                'test@project_name',
+                'test',
+            )
+            self.stdout.write(
+                'Superuser created:\n'
+                '\tname: test\n'
+                '\temail: test@project_name\n'
+                '\tpassword: test'
+            )
