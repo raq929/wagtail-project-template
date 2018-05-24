643ad9e03231ab197182ad9baa441fdc8d75e78a
racheltstevens <racheltstevens@gmail.com>
2018-05-24 15:28:47 -0400

    Appease flake8

--- project_name/urls.py
+++ project_name/urls.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf import settings
 from django.conf.urls import url, include
-from django.urls import path, re_path
+from django.urls import path
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
